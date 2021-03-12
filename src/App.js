212aaaefc417a2b7f55eb08ab62e59498003deb3
mitchellkealy <60372669+mitchellkealy@users.noreply.github.com>
2021-03-12 11:16:49 -0600

    Added a workig navbar, although doesn't look great.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
 import { MemoryRouter, Switch, Route } from 'react-router-dom';
 
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import React, { Component } from 'react'; 
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, NavbarBrand } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
@@ -16,8 +19,41 @@ const About = () => <span>About</span>;
 const Users = () => <span>Users</span>;
 
 const App = () => (
+  
   <MemoryRouter>
-    <Container className="p-3">
+    
+  <>
+  <Container>
+  <Navbar bg="light" variant="light">
+    <Navbar.Brand href="#home">tca-kingdomino</Navbar.Brand>
+    <Nav className="mr-auto">
+    
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      
+            <LinkContainer to="/">
+            <li class="nav-item">
+              <a class="nav-link active">Home</a>
+            </li>
+            </LinkContainer>
+      
+      <li class="nav-item">     
+            <LinkContainer to="/about">
+              <a class="nav-link active">About</a>
+            </LinkContainer>
+      </li>
+      <li class="nav-item"> 
+            <LinkContainer to="/users">
+              <a class="nav-link active">Users</a>         
+            </LinkContainer>
+      </li>       
+    </ul> 
+    </Nav>
+  </Navbar>
+  
+</Container>
+  </>
+
+<Container className="p-3">
       <Jumbotron>
         <h1 className="header">Welcome To React-Bootstrap</h1>
         <h2>
@@ -33,20 +69,6 @@ const App = () => (
               <Home />
             </Route>
           </Switch>
-        </h2>
-        <h2>
-          Navigate to{' '}
-          <ButtonToolbar className="custom-btn-toolbar">
-            <LinkContainer to="/">
-              <Button>Home</Button>
-            </LinkContainer>
-            <LinkContainer to="/about">
-              <Button>About</Button>
-            </LinkContainer>
-            <LinkContainer to="/users">
-              <Button>Users</Button>
-            </LinkContainer>
-          </ButtonToolbar>
         </h2>
       </Jumbotron>
     </Container>
